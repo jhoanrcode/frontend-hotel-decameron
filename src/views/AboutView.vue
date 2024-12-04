343d3a798c4a0b3ede17fff11da2ba23e4c118bc
Jhoan Avila <108818345+jhoanrcode@users.noreply.github.com>
2024-12-04 16:50:17 -0500

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,7 +8,10 @@ import CardBlock from '@/components/layout/CardBlock.vue'
             <li>⚙️ <b>Code:</b> Vite + Vue 3 + JavaScript<br>PHP + Laravel 11 <br> PostgresSQL</li>
             <li>📱 <b>Telefono:</b> 3203645490</li>
             <li>📧 <b>Email:</b> <a href="mailto:joria94@hotmail.com">joria94@hotmail.com</a></li>
-            <li>🛠️ <b>Github:</b> <a href="https://github.com/jhoanrcode" target="_blank">Repositorio proyecto</a></li>
+            <li>🛠️ <b>Github:</b> 
+                <a href="https://github.com/jhoanrcode/frontend-hotel-decameron" target="_blank"> Repositorio Frontend</a> - 
+                <a href="https://github.com/jhoanrcode/apirest-hotel-decameron" target="_blank">Repositorio API</a>
+            </li>
         </ul>
     </CardBlock>
 </template>
