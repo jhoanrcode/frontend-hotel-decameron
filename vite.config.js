2fc8e0200839dbc5ec50e0b970f1e3b317261983
Jhoan Avila <108818345+jhoanrcode@users.noreply.github.com>
2024-12-04 17:18:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: "https://jhoanrcode.github.io/frontend-hotel-decameron/",
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default ({ mode }) => {
+
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+      base: "/frontend-hotel-decameron/",
+    
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+      server: {
+        port: parseInt(process.env.VITE_PORT) || 3001,
+
+      }
+    })
+};
