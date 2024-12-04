ddb1bf9fea19ff93bbf6199dd724fe6857a64fb7
Jhoan Avila <108818345+jhoanrcode@users.noreply.github.com>
2024-12-04 17:25:31 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory("/frontend-hotel-decameron/"),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 });
 
